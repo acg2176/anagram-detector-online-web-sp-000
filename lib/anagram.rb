7465ef0685abce70f14f6daf22216ebf21d0f530
Anna Guerrero <acg2176@columbia.edu>
2020-04-18 17:14:02 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,8 +6,17 @@ class Anagram
     @word = word
   end
 
-  def match(array)
-    
+  def match(words)
+    anagrams = []
+    orig_word = self.split("") #original word split into letters, an array
+    #array = words.split(" ") #array of words
+    words.each do |elem| #elem is word in array
+      letters = elem.split("") #array of letters
+      if letters.sort == orig_word.sort #if true
+        anagrams << elem #add that word to the new array
+      end
+    end
+    anagrams
   end
 
 
